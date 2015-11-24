42c166cb99ff2872dcf8b4f370864b52f1fd0882
Vladislav Zavadski <vladislav.zavadski@gmail.com>
2015-11-24 23:53:13 +0200

    some refactoring

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -13,36 +13,14 @@ import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
+import java.net.InetAddress;
 
 public class Main extends Application {
 
 
     @Override
     public void start(Stage stage) throws IOException {
-        Group root = new Group();
-        Separator separatorLine = new Separator();
-        ScrollBar sb = new ScrollBar();
-        separatorLine.setOrientation(Orientation.HORIZONTAL);
-        separatorLine.setMinWidth(800);
-        sb.setOrientation(Orientation.VERTICAL);
-        sb.setTranslateX(780);
-        sb.setTranslateY(70);
-        sb.setMinHeight(500);
-        sb.setMin(0);
-        sb.setOrientation(Orientation.VERTICAL);
-        sb.setPrefHeight(180);
-        sb.setMax((480)*3);
-        separatorLine.setTranslateY(70);
-        String recense = "Марсианская миссия «Арес-3» в процессе работы была вынуждена экстренно покинуть планету из-за надвигающейся песчаной бури. Инженер и биолог Марк Уотни получил повреждение скафандра во время песчаной бури. Сотрудники миссии, посчитав его погибшим, эвакуировались с планеты, оставив Марка одного. Очнувшись, Уотни обнаруживает, что связь с Землёй отсутствует, но при этом полностью функционирует автономный Дом. Главный герой начинает искать способ продержаться на имеющихся запасах еды, витаминов, воды и воздуха ещё 4 года до прилёта следующей миссии «Арес-4». В это время на Земле NASA ищет способ как можно скорее отправить спасательную миссию на красную планету.";
-        FilmView filmView = new FilmView("Марсианин", "https://image.tmdb.org/t/p/w185/sQdalmBSUiaU0QCgZKBfy0l2vUR.jpg", "18/10/2015", "100000000", "100000000",recense, FXCollections.observableArrayList(
-                "Single", "Double", "Suite"), 9);
-        WindowTop top = new WindowTop();
-        VBox films = new VBox(40);
-        films.getChildren().addAll(filmView, new FilmView("Марсианин", "https://image.tmdb.org/t/p/w185/AjbENYG3b8lhYSkdrWwlhVLRPKR.jpg", "18/10/2015", "100000000", "100000000",recense, FXCollections.observableArrayList(
-                "Single", "Double", "Suite"), 4), new FilmView("Белорусианин", "https://image.tmdb.org/t/p/w185/AjbENYG3b8lhYSkdrWwlhVLRPKR.jpg", "18/10/2015", "100000000", "100000000",recense, FXCollections.observableArrayList(
-                "Single", "Double", "Suite"), 4), new FilmView("Марсианин", "https://image.tmdb.org/t/p/w185/al0BqCrBAeMaWflvFKt3mZzTvXS.jpg", "18/10/2015", "100000000", "100000000",recense, FXCollections.observableArrayList(
-                "Single", "Double", "Suite"), 4));
-        root.getChildren().addAll(films, separatorLine, sb, top);
+        MainForm root = new MainForm();
         Scene scene = new Scene(root);
         stage.setResizable(false);
         stage.setWidth(800);
@@ -50,10 +28,6 @@ public class Main extends Application {
         stage.setTitle("Film master");
         stage.setScene(scene);
         stage.show();
-        System.out.print(0b10);
-        sb.valueProperty().addListener((ov, old_val, new_val) -> {
-            films.setLayoutY(-new_val.doubleValue());
-        });
     }
 
     public static void main(String[] args) {
---
+++ src/sample/MainForm.java
@@ -0,0 +1,84 @@
+package sample;
+
+import javafx.application.Platform;
+import javafx.geometry.Orientation;
+import javafx.scene.Group;
+import javafx.scene.control.ScrollBar;
+import javafx.scene.control.Separator;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+import java.io.File;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+/**
+ * Created by Владислав on 24.11.2015.
+ */
+public class MainForm extends Group {
+    private WindowTop windowTop;
+    private Separator separatorLine;
+    private ScrollBar sb;
+    private Image image;
+    private ImageView connectionLost;
+    private VBox films;
+    public MainForm(){
+        windowTop = new WindowTop();
+        try {
+            if("127.0.0.1".equals(InetAddress.getLocalHost().getHostAddress().toString())){
+                image = new Image(new File("resourse/lost.png").toURI().toString());
+                connectionLost = new ImageView(image);
+                connectionLost.setTranslateY(140);
+                connectionLost.setTranslateX(220);
+                getChildren().addAll(connectionLost);
+                windowTop.setDisable(true);
+                Thread thread = new Thread(){
+                  public void run(){
+                      try {
+                          while("127.0.0.1".equals(InetAddress.getLocalHost().getHostAddress().toString())){
+
+                          }
+                          Platform.runLater(new Thread(){
+                              @Override
+                              public void run(){
+                                  getChildren().remove(connectionLost);
+                              }
+
+
+                          });
+                          windowTop.setDisable(false);
+                       } catch (UnknownHostException e) {
+                          e.printStackTrace();
+                      }
+                  }
+                };
+                thread.start();
+            }
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        }
+
+        separatorLine = new Separator();
+        sb = new ScrollBar();
+        separatorLine.setOrientation(Orientation.HORIZONTAL);
+        separatorLine.setMinWidth(800);
+        sb.setOrientation(Orientation.VERTICAL);
+        sb.setTranslateX(780);
+        sb.setTranslateY(70);
+        sb.setMinHeight(500);
+        sb.setMin(0);
+        sb.setOrientation(Orientation.VERTICAL);
+        sb.setPrefHeight(180);
+        sb.setMax((480)*3);
+        separatorLine.setTranslateY(70);
+        films = new VBox(40);
+        getChildren().addAll(separatorLine, windowTop);
+        sb.valueProperty().addListener((ov, old_val, new_val) -> {
+            films.setLayoutY(-new_val.doubleValue());
+        });
+    }
+    public void fun1(){
+        getChildren().remove(connectionLost);
+    }
+}
